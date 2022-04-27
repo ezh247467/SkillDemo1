a68579e44093e9a0e42be174388d56e5dd9543b3
ezh247467 <e8huang@ucsd.edu>
2022-04-26 19:27:40 -0700

    mod add.java added addTest.java and imported library

--- add.java
+++ add.java
@@ -2,3 +2,8 @@
 javac -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar addTest.java
 java -cp .:lib/junit-4.13.2.jar:lib/hamcrest-core-1.3.jar org.junit.runner.JUnitCore addTest
 */
+public class add {
+    public static int addTwo(int a, int b) {
+        return a * b;
+    }
+}
---
+++ addTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.*;
+public class addTest {
+    @Test
+    public void addTwo() {
+        assertEquals(5, add.addTwo(2, 3));
+    }
+}
